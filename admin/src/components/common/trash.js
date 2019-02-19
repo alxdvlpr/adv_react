218322cf4e9b8de1df7515691b4f0f2e876a6c5e
Anna Vostrykova <av@audienceproject.com>
2019-02-19 19:06:55 +0100

    HomeWork 4: Use img for trash

--- admin/src/components/common/trash.js
+++ admin/src/components/common/trash.js
@@ -9,21 +9,34 @@ class Trash extends Component {
 
   render() {
     const { dropTarget, canDrop, isOver } = this.props
-    const border = `3px solid ${canDrop ? (isOver ? 'red' : 'green') : 'black'}`
+    const fill = canDrop ? (isOver ? 'red' : 'green') : 'black'
 
     return dropTarget(
       <div
         style={{
-          border,
           width: 200,
           height: 200,
           position: 'absolute',
           top: 20,
           right: 20
         }}
-      />
+      >
+        {this.getSvg(fill)}
+      </div>
     )
   }
+
+  getSvg = (fill) => (
+    <svg
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+    >
+      <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
+    </svg>
+  )
 }
 
 const spec = {
